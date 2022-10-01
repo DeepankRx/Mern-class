2fe7a2cd7db48aa0eeaa25186b0c3507edab0f0f
DeepankRx <deepankpushpad10@gmail.com>
2022-10-01 20:51:28 +0530

    delete functionality

--- frontend/src/AddToDo.js
+++ frontend/src/AddToDo.js
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddToDo = () => {
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [list, setList] = useState([]);
+  const addToList = () => {
+    if (title.length < 3 || desc.length < 3) {
+      alert('Title and desc should be more than 3');
+      return;
+    }
+    const newInput = {
+      title: title,
+      desc: desc,
+      id: Math.floor(Math.random() * 1000),
+    };
+
+    setList([...list, newInput]);
+    setTitle('');
+    setDesc('');
+    console.log(list);
+  };
+  const deleteItem = (id) => {
+    const updateList = list.filter((item) => {
+      return item.id !== id;
+    });
+    setList(updateList);
+  };
   return (
     <div>
-      <label>Title</label>
-      <input type="text" />
-      <label>Description</label>
-      <input type="text" />
-      <button>Add ToDo</button>
+      <div>
+        <label>Title</label>
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label>Description</label>
+        <input
+          type="text"
+          onChange={(e) => setDesc(e.target.value)}
+          value={desc}
+        />
+        <button onClick={() => addToList()}>Add ToDo</button>
+      </div>
+      <div>
+        <ul>
+          {list.map((item) => {
+            return (
+              <li key={item.id}>
+                {item.title} {item.desc}
+                <button>Edit</button>
+                <button
+                  onClick={() => {
+                    deleteItem(item.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
--- frontend/src/Example.js
+++ frontend/src/Example.js
@@ -1,26 +1,23 @@
 import React, { useState } from 'react';
 
-// const Example = () => {
-//   //const [x,setX] = useState('')
-//   const [name, setName] = useState('');
-//   return (
-//     <div>
-//       <label>My Name Is : {name}</label>
-//       <br />
-//   <input type="text" onChange={(e) => setName(e.target.value)} />
-//     </div>
-//   );
-// };
-
 const Example = () => {
-  const [count, setCounter] = useState(0);
+  const [count, setCount] = useState(0);
+  const addByOne = () => {
+    setCount(count + 1);
+    if (count % 5 == 0) alert('Number is multiple of 5');
+  };
   return (
     <div>
-      Counter : {count}
-      <br />
-      <button onClick={() => setCounter(count + 1)}>Increase By 1</button>
-      <button onClick={() => setCounter(count - 1)}>Decrease By 1</button>
-      <button onClick={() => setCounter(0)}>Set To 0</button>
+      <h1>Example</h1>
+      <button
+        onClick={() => {
+          addByOne();
+        }}
+      >
+        +
+      </button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count - 1)}>-</button>
     </div>
   );
 };
